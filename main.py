ce7d0107e8ef8c4ea0ebf5e0ee087c9fdecc2e33
Vlad <somchenko96@gmail.com>
2026-02-07 18:34:10 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,103 @@
+from collections import UserDict
+
+class Field:
+	def __init__(self, value):
+		self.value = value
+
+	def __str__(self):
+		return str(self.value)
+
+class Name(Field):
+		pass
+
+class Phone(Field):
+		pass
+
+class Record:
+	def __init__(self, name):
+		self.name = Name(name)
+		self.phones = []
+
+	def add_phone(self, phone):
+		self.phones.append(Phone(phone))
+
+	def remove_phone(self, phone):
+		phone_obj = self.find_phone(phone)
+		if phone_obj:
+			self.phones.remove(phone_obj)
+
+	def edit_phone(self, old_phone, new_phone):
+		try:
+			for i, phone in enumerate(self.phones):
+				if phone.value == old_phone:
+					self.phones[i] = Phone(new_phone)
+					return
+			print("Phone not found")
+		except ValueError:
+			print("Incorrect number")
+
+	def find_phone(self, number):
+		for phone in self.phones:
+			if phone.value == number:
+				return phone
+		return None
+
+	def __str__(self):
+		return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+
+class AddressBook(UserDict):
+
+	def add_record(self, record):
+		self.data[record.name.value] = record
+
+	def find(self, name):
+		return self.data.get(name)
+
+	def delete(self, name):
+		self.data.pop(name, None)
+
+	def __str__(self):
+		if not self.data:
+			return "Address book is empty."
+
+		result = []
+		for record in self.data.values():
+			result.append(str(record))
+
+		return "\n".join(result)
+
+
+# Створення нової адресної книги
+book = AddressBook()
+
+# Створення запису для John
+john_record = Record("John")
+john_record.add_phone("1234567890")
+john_record.add_phone("5555555555")
+
+# Додавання запису John до адресної книги
+book.add_record(john_record)
+
+# Створення та додавання нового запису для Jane
+jane_record = Record("Jane")
+jane_record.add_phone("9876543210")
+book.add_record(jane_record)
+
+# Виведення всіх записів у книзі
+
+print(book)
+
+# Знаходження та редагування телефону для John
+john = book.find("John")
+john.edit_phone("1234567890", "1112223333")
+
+print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
+
+# Пошук конкретного телефону у записі John
+found_phone = john.find_phone("5555555555")
+print(f"{john.name}: {found_phone}")  # Виведення: John: 5555555555
+
+# Видалення запису Jane
+book.delete("Jane")
+
+print(book)
