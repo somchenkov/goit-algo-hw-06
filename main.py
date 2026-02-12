77affcb9b20709b5b82fe5e891b184fb35986db0
Vlad <somchenko96@gmail.com>
2026-02-12 11:22:25 -0500

    check new phone number before removing the old one

--- main.py
+++ main.py
@@ -37,6 +37,8 @@ class Record:
 	def edit_phone(self, old_phone, new_phone):
 		self.remove_phone(old_phone)
 		self.add_phone(new_phone)
+		self.remove_phone(old_phone)
+
 
 
 	def find_phone(self, number):
